1cd48871ca4d1886c40a4991fe1b0548d6524be3
Rahatul-Alom <muhammadrahatulalom@gmail.com>
2023-09-16 21:03:30 +0600

    added credit hour remainig option

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,13 +1,11 @@
 // eslint-disable-next-line react/prop-types
-const Cart = ({selectedCourse}) => {
-    console.log(selectedCourse)
-    
+const Cart = ({selectedCourse, remainig}) => {
     return (
-        <div>
-            <h2 className="text-xl font-bold mb-3 text-blue-500">Credit Hour Remaining </h2>
+        <div className="px-2">
+            <h2 className="text-xl font-bold mb-3 text-blue-900">Credit Hour Remaining {remainig} hr </h2>
             <hr />
             <h2 className="text-xl font-bold my-3">Course Name</h2>
-            <ol className="list-decimal list-inside px-2 text-sm">
+            <ol className="list-decimal list-inside text-sm">
             {
             // eslint-disable-next-line react/prop-types
             selectedCourse.map((course,keys) =>(
@@ -17,6 +15,7 @@ const Cart = ({selectedCourse}) => {
             }
             </ol>
             <hr className="mt-3" />
+            <p className="my-4">Total Credit Hour : </p>
         </div>
     );
 };
--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -7,6 +7,7 @@ import Cart from "../cart/Cart";
 const Courses = () => {
   const [courses, setCourse] = useState([]);
   const [selectedCourse, setSelectedCourse] = useState([])
+  const [remainig, setRemainig] = useState([])
 
   useEffect(() => {
     fetch("./data.json")
@@ -16,10 +17,18 @@ const Courses = () => {
 
   const handleSelectCourse = (course) =>{
     const isExist = selectedCourse.find((item) => item.id === course.id)
+    let count = course.credit
     if(isExist){
       alert('eta ase')
     }
     else{
+      selectedCourse.forEach((item) => {
+        count = count + item.credit
+      });
+      console.log(count)
+      const totalRemainig = 20-count;
+      setRemainig(totalRemainig);
+      console.log(totalRemainig);
       setSelectedCourse([...selectedCourse, course])
     }
   }
@@ -32,7 +41,7 @@ const Courses = () => {
                 <div key={course.id} className=" rounded-xl shadow-xl p-4">
                 <img className="w-full" src={course.image} alt=""></img>
                 <div className="py-4">
-                    <h2 className=" font-bold text-xl mb-2 text-center">{course.course_name}</h2>
+                    <h2 className=" font-bold text-lg mb-2">{course.course_name}</h2>
                     <p className="text-gray-700 text-base text-justify">
                         {course.description}
                     </p>
@@ -43,7 +52,7 @@ const Courses = () => {
                         </div>
                         <div className="flex items-center gap-2 mt-2">
                             <BiBookOpen/>
-                            <p>Credit: {course.credit}</p>
+                            <p>Credit: {course.credit}hr</p>
                         </div>
                     </div>
                     <div className="text-center">
@@ -55,7 +64,7 @@ const Courses = () => {
         }
         </div>
       <div className="bg-gray-500 rounded-xl p-2 mt-10 w-[700px]">
-      <Cart selectedCourse = {selectedCourse}></Cart>
+      <Cart selectedCourse = {selectedCourse} remainig = {remainig}></Cart>
       </div>  
 
    </div>
